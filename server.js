454f078e58e13c59cac3d33d19b9570384b8c0d5
Stewart Jester <jesterstewart@gmail.com>
2020-08-22 20:32:18 -0700

    prepped for deploy

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const cors = require('cors')
 const app = express()
 const movies = require('./moviedata.json')
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan('morganSetting'))
 app.use(helmet())
 app.use(cors())
 
@@ -16,7 +17,7 @@ app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
 
-    console.log('validate bearer token middleware')
+    
 
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({ error: 'Unauthorized request' })
@@ -54,8 +55,18 @@ app.get('/movie' , function handleGetMovies(req, res) {
   res.json(response)
   })
 
-const PORT = 8000
+  app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+  
 })
